db291da2c6d9f64b0f4c716e4865ec6677c6fedc
joel071 <117740794+joel071@users.noreply.github.com>
2022-11-25 14:34:25 +0100

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+$iduser = $_SESSION['iduser'];
+$selectBar = filter_input(INPUT_POST, "selectBar", FILTER_VALIDATE_INT);
+$selectBar1 = filter_input(INPUT_POST, "selectBar1", FILTER_VALIDATE_INT);
+$id= $_POST['id'];
+$NBpage=$_POST['NBpage'];
+$equality='1';
+$retour=$NBpage.'-matchs.html';
+$equality='1';
+$host = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "match";
+
+    // creating a connection
+    $con = mysqli_connect($host, $username, $password, $dbname);
+
+    // to ensure that the connection is made
+    if (!$con)
+    {
+        die("Connection failed!" . mysqli_connect_error());
+    }
+
+    // using sql to create a data entry query
+  
+    $sql = "INSERT INTO matches (equipe1,equipe2,idMatch, iduser) VALUES ('$selectBar','$selectBar1','$id','$iduser')"; 
+
+    // send query to the database to add values and confirm if successful
+    $rs = mysqli_query($con, $sql);
+    if($rs)
+    {
+        header("Location: $retour");
+    }
+  
+    // close connection
+
+?>
---
+++ navbar.php
@@ -0,0 +1,19 @@
+                  <span>14</span>
+                  <nav class="navbar">
+      <span class="navbar-toggle" id="js-navbar-toggle">
+              <i class="fas fa-bars"></i>
+          </span>
+      <a href="#" class="logo">Pronostics</a>
+      <ul class="main-nav" id="js-menu">
+        <li>
+          <a href="#" class="nav-links">Home</a>
+        </li>
+        <li>
+          <a href="Calendar.html" class="nav-links">Calendar</a>
+        </li>
+        <li>
+          <a href="logout.php" class="nav-links">Log Out</a>
+        </li>
+       
+      </ul>
+    </nav>
